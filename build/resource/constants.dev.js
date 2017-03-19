725b4118974a62288222b8c37658c2a638d5eda6
lijun.zhou <zhouj.172@gmail.com>
2017-03-19 15:06:59 +0800

    3-18

--- build/resource/constants.dev.js
+++ build/resource/constants.dev.js
@@ -6,8 +6,8 @@
 "use strict"
 module.exports = {
 //  INTERFACE_URL : "http://rap.taobao.org/mockjsdata/3392/", //接口请求路径
-	INTERFACE_URL : "http://localhost:8081/", //接口请求路径
-//	INTERFACE_URL : "http://106.14.80.129:9090/", //接口请求路径
+//	INTERFACE_URL : "http://localhost:8081/", //接口请求路径
+	INTERFACE_URL : "http://106.14.80.129:90/", //接口请求路径
     TOKEN:'param-token',
     debug : 1,
     dev:true,
